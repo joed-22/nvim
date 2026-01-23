6455f933ea85cfea907f04ed8908de0706abb682
joed-22 <joedeflorio22@gmail.com>
2026-01-23 15:37:58 -0500

    enable current line git blame by default

--- lua/plugins/gitsigns.lua
+++ lua/plugins/gitsigns.lua
@@ -1,3 +1,10 @@
 return {
-  "lewis6991/gitsigns.nvim"
+  "lewis6991/gitsigns.nvim",
+  opts = {
+    current_line_blame = true,
+    current_line_blame_opts = {
+      delay = 300,          -- ms
+    },
+  },
+
 }
