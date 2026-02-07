b877e146af2bdb8347bdcfd612281a7d8cacee01
joed <joedeflorio22@gmail.com>
2026-02-07 03:26:46 -0500

    winbar for vsplit

--- lua/plugins/lualine.lua
+++ lua/plugins/lualine.lua
@@ -1,3 +1,15 @@
+local function has_multiple_real_buffers()
+  local count = 0
+  for _, win in ipairs(vim.api.nvim_tabpage_list_wins(0)) do
+    local bufnr = vim.api.nvim_win_get_buf(win)
+    local bt = vim.bo[bufnr].buftype
+    if bt == "" then
+      count = count + 1
+    end
+  end
+  return count > 1
+end
+
 return {
   "nvim-lualine/lualine.nvim",
   dependencies = { "nvim-tree/nvim-web-devicons" },
@@ -16,5 +28,44 @@ return {
       lualine_y = { "progress" },
       lualine_z = { "location" },
     },
+
+    winbar = {
+      lualine_c = {
+        {
+          function()
+
+            -- Show 3 dirs back...
+
+
+            local max_dirs = 3
+            local sep = "  "
+
+            local path = vim.fn.expand("%:p:h")
+            local file = vim.fn.expand("%:t")
+            -- if no path or not a file nothing...
+            if path == "" or vim.bo.buftype ~= "" or not has_multiple_real_buffers() then
+              return ""
+            end
+
+            local parts = vim.split(path, "/", { trimempty = true })
+            parts = vim.list_slice(parts, math.max(#parts - max_dirs + 1, 1))
+
+            table.insert(parts, file)
+            return table.concat(parts, sep)
+          end,
+          color = "StatusLine",
+        },
+      },
+    },
+    
+    inactive_winbar = {
+      lualine_c = {
+        {
+          'filename',
+          cond = is_real_file,
+        },
+      },
+    },
+
   },
 }
