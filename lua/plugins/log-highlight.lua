621cc1a1a57f79cbb10f2863d07aef3a17d8796d
joed-22 <joedeflorio22@gmail.com>
2026-09-02 21:43:39 -0400

    log-highligh with pipe in text

--- lua/plugins/log-highlight.lua
+++ lua/plugins/log-highlight.lua
@@ -8,4 +8,19 @@ return
           'Downloads%/logs%/*'
       },
     },
+    init = function()
+      -- :Log -- force log highlighting on the current buffer
+      vim.api.nvim_create_user_command('LogColor', function()
+        vim.bo.filetype = 'log'
+      end, { desc = 'Highlight current buffer as a log file' })
+
+      -- anything piped in on stdin (journalctl -b | nvim -) gets it automatically
+      vim.api.nvim_create_autocmd('StdinReadPost', {
+        callback = function()
+          if vim.bo.filetype == '' then
+            vim.bo.filetype = 'log'
+          end
+        end,
+      })
+    end,
 }
